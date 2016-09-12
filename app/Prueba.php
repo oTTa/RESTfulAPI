5ccfb4a2ff4e94e6d9d02c55a9790e37e7b9aa4c
oTTa <lucaotarola@gmail.com>
2016-09-12 19:56:37 -0300

    Primer commit
    Se envia la estructura completa de Laravel 5

---
+++ app/Prueba.php
@@ -0,0 +1,10 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Prueba extends Model
+{
+        public function saludar ($nombre){
+            return ("Hola $nombre");
+        }
+}
