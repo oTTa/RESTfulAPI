02ca6c1850a91471bc65fe4263195f4b97fc4807
oTTa <lucaotarola@gmail.com>
2016-09-13 17:32:29 -0300

    Se crean los seeder y las routas REST
    - Se crea cada clase para llenar con datos falsos la bd y se generan las
    ruta rest para la app

---
+++ database/seeds/VehiculoSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Vehiculo;
+use App\Fabricante;
+use Faker\Factory as Faker;
+
+class VehiculoSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{   
+            $faker = Faker::create();
+            $cantidad = Fabricante::all()->count();
+            
+            for ($j=0; $j<$cantidad; $j++){
+                for ($i=0; $i<3 ; $i++)
+                {
+                    Vehiculo::create
+                        ([
+                            'color' => $faker->safeColorName(),
+                            'cilindraje' => $faker->randomFloat(2),
+                            'potencia' => $faker->randomNumber(3),
+                            'peso' => $faker->randomFloat(2),
+                            'fabricante_id' => $faker->numberBetween(1, $cantidad)
+                        ]);
+                }
+            }
+	}
+
+}
+
